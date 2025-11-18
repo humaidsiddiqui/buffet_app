98b27a22a107bfa537a11fe77385af8021792c79
Humaid Siddiqui <humaidsiddiqui81@gmail.com>
2025-11-18 03:20:55 -0800

    routes are fixed

---
+++ app/routes.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/buffets")
+def buffets():
+    return render_template("buffets.html")
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+@app.route("/booknow")
+def book_now():
+    return render_template("booknow.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
--- app/run.py
+++ app/run.py
@@ -6,5 +6,39 @@ app = Flask(__name__)
 def index():
     return render_template('base.html')
 
+@app.route("/buffets")
+def buffets():
+    return render_template("buffets.html")
+
+@app.route("/pricing&hours")
+def login():
+    return render_template("pricing.html")
+
+@app.route("/reservations")
+def about():
+    return render_template("reservations.html")
+
+@app.route("/location")
+def contact():
+    return render_template("location.html")
+
+@app.route("/bookatable")
+def book_now():
+    return render_template("bookatable.html")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True) 
