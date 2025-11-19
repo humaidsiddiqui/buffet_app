c3f932b89def9c7ad3083ff2beafc0118eb63dab
Humaid Siddiqui <humaidsiddiqui81@gmail.com>
2025-11-18 19:56:36 -0800

    login html page

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -1,9 +1,9 @@
-async function initMap() {
-    // Create a map centered at a specific location
-    const map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 8,
-        center: { lat: -34.397, lng: 150.644 },
-        colorScheme: ColorScheme.DARK,
-        mapId: "YOUR_MAP_ID"
-    });
-}
+// async function initMap() {
+//     // Create a map centered at a specific location
+//     const map = new google.maps.Map(document.getElementById("map"), {
+//         zoom: 8,
+//         center: { lat: -34.397, lng: 150.644 },
+//         colorScheme: ColorScheme.DARK,
+//         mapId: "YOUR_MAP_ID"
+//     });
+// }
