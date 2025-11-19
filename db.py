d3d967648d38789234d5936de80d1a933c418bb4
Humaid Siddiqui <humaidsiddiqui81@gmail.com>
2025-11-19 02:41:01 -0800

    Removed sensitive db.py from repo

--- db.py
+++
@@ -1,9 +0,0 @@
-import psycopg2
-def get_db():
-    return psycopg2.connect(
-        dbname="Buffetfinder",
-        user="humaidsiddiqui",
-        password="P@ssword500",
-        host="localhost",
-        port="5432"
-    )
